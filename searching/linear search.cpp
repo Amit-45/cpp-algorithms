db63e736643a9d44799ccbf9e1ac8e81e7dc9249
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2021-04-26 18:10:00 +0530

    Update and rename linear search to linear search.cpp

---
+++ searching/linear search.cpp
@@ -0,0 +1,34 @@
+
+//Linear search
+//Take input of size and key(element to be )
+//The linearSearch function returns the index value of the key if the key is found else returns the value -1
+
+#include <bits/stdc++.h>
+using namespace std;
+int linearSearch(int a[], int size, int key)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (a[i] == key)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+
+    int size;
+    cin >> size;
+    int a[size];
+    int key;
+    cin >> key;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> a[i];
+    }
+    cout << linearSearch(a, size, key) << "\n";
+}
