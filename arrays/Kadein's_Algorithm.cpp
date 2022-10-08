de278aeee7ece336570ec64d305af2d0004ce132
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-08 23:18:45 +0530

    Update Kadein's_Algorithm.cpp

--- arrays/Kadein's_Algorithm.cpp
+++ arrays/Kadein's_Algorithm.cpp
@@ -3,6 +3,30 @@
 //Used to find largest sum contiguous subarray
 //Takes negative into consideration too
 
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        //KADANES ALGO 
+        int sum = 0;
+        int maxi=nums[0]; //because atleast there is one solution
+
+        for(int i=0;i<nums.size();i++)
+        {
+            //STEP1
+            sum=sum+nums[i];
+            //STEP2
+            maxi=max(maxi,sum);
+            //STEP3
+            if(sum<0) sum=0;
+        }
+       
+        return maxi;
+    }
+};
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
