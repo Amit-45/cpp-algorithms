c007ac1088cd0013cb8587675f42b0f7a16a02fc
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-15 07:29:33 +0530

    Update selection sort.cpp

--- sorting/selection sort.cpp
+++ sorting/selection sort.cpp
@@ -28,3 +28,53 @@ int main()
 
     return 0;
 }
+
+------------------------------OTHER WAY---------------------------------------------------------------------------------------------
+
+
+
+void swap(int arr[], int firstIndex, int secondIndex) 
+{   
+    int temp;
+    temp = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
+}
+int indexOfMinimum(int arr[], int startIndex, int n) {
+    int minValue = arr[startIndex];
+    int minIndex = startIndex;
+    int i;
+    for(i = minIndex + 1; i < n; i++){
+        if(arr[i] < minValue) 
+        {
+            minIndex = i;
+            minValue = arr[i];
+        }
+    } 
+    return minIndex;
+}
+void selectionSort(int arr[], int n) 
+{
+    int i;
+    for(i = 0; i < n; i++) 
+    {
+        int index = indexOfMinimum(arr, i, n);
+        swap(arr, i, index);
+    }
+}
+void printArray(int arr[], int size)
+{
+    int i;
+    for(i = 0; i < size; i++)
+            cout<<arr[i]<<" ";
+    }
+int main()
+{
+    int arr[100],n,i;
+    cin>>n;
+    for(i=0;i<n;i++)
+         cin>>arr[i];
+    selectionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
