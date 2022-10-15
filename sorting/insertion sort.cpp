e37ca1f0a87c89b3b165b760359ab435ebc2a67d
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2022-10-15 07:22:35 +0530

    Update insertion sort.cpp

--- sorting/insertion sort.cpp
+++ sorting/insertion sort.cpp
@@ -31,3 +31,35 @@ int main()
 
     return 0;
 }
+
+
+//Another way 
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int n,temp,i,j; 
+   cin>>n;
+  int data[n];
+    for(i=0;i<n;i++) 
+    {
+        cin>>data[i];
+    } 
+    for(i=1;i<n;i++) 
+    { 
+        temp = data[i];
+        j=i-1; 
+        while(temp<data[j] && j>=0) 
+        { 
+            data[j+1] = data[j]; 
+            --j; 
+        } 
+        data[j+1]=temp; 
+    } 
+    
+    for(i=0; i<n; i++) 
+        cout<<data[i]<<" "; 
+    return 0; 
+}
